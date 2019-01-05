482a2c2f7fec44b92009fe602f6ea123198cf9e9
arrfour <arr.four@gmail.com>
2019-01-04 22:30:16 -0500

    updated install scripting

--- install.sh
+++ install.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-rm ~/.bashrc
-rm ~/.bash_aliases
-rm ~/.bash_exports
-rm ~/.bash_wrappers
-rm ~/.vimrc
-rm ~/.screenrc
-rm ~/.tmux.conf
-ln -s dotfiles/.bashrc ~/
-ln -s dotfiles/.bash_aliases ~/
-ln -s dotfiles/.bash_exports ~/
-ln -s dotfiles/.vimrc ~/
-ln -s dotfiles/.screenrc ~/
-ln -s dotfiles/.tmux.conf ~/
+# Find all dot files then if the original file exists, create a backup
+# Once backed up to {file}.dtbak symlink the new dotfile in place
+for file in $(find . -maxdepth 1 -name ".*" -type f  -printf "%f\n" ); do
+    if [ -e ~/$file ]; then
+        mv -f ~/$file{,.dtbak}
+    fi
+    ln -s $PWD/$file ~/$file
+done
+
+# Check if vim-addon installed, if not, install it automatically
+if hash vim-addon  2>/dev/null; then
+    echo "vim-addon (vim-scripts)  installed"
+else
+    echo "vim-addon (vim-scripts) not installed, installing"
+    sudo apt update && sudo apt -y install vim-scripts
+fi
+
+echo "Installed"
